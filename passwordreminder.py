f6bb0e91d32d9da188e30c7c0237e48ad7c38666
Mahir Durmaz <82589231+mahirdurmaz@users.noreply.github.com>
2021-04-18 23:16:22 +0300

    Add files via upload

---
+++ passwordreminder.py
@@ -0,0 +1,13 @@
+name = "Mahir"
+
+newname = str(input("Please enter your name : ")).title().strip().upper().capitalize()
+
+
+
+if newname == name :
+
+    print("Hello", newname, "your password is 7dKo133NfaQ2")
+
+else : 
+
+    print("Hello", newname, "see you later ;)")
