0d6773d9777b9164625614ad295811987aec0425
Mahir Durmaz <82589231+mahirdurmaz@users.noreply.github.com>
2021-04-29 20:24:50 +0300

    Add files via upload

---
+++ armstong_number.py
@@ -0,0 +1,15 @@
+while True :
+    number = input("Enter a positive number:")
+    digits = len(number)
+    summ = 0
+    if not number.isdigit() :
+        print(number, "is invalid entry. Enter numberic value!")
+    elif int(number) >= 0 :
+        for i in range(digits) :
+            summ = summ + int(number[i]) ** digits
+        if summ == int(number) :
+            print(number, "is an Armstrong Number.")
+            break
+        else :
+            print(number, "is not an Armstrong Number.")
+            break
